521cefaf0292f0ecfe5fb575a02ca8845a1f3af6
Alessandro Rodrigo <88581684+alebarreto1@users.noreply.github.com>
2021-08-30 09:27:07 -0300

    tiacademy

--- exercíciosExtra/LISTA1/exercício10-Lista1/script.js
+++
@@ -1,9 +0,0 @@
-/* Faça um programa que calcule e mostre a
-área de um círculo. Sabe-se que: Área = Pi * R2, aonde Pi = 3,14. */
-
-const Pi = 3.14;
-var raio = parseFloat(prompt("Insira o raio: "));
-
-var area = (Pi * (raio**2)).toFixed(2);
-
-document.write("Área do círculo: "+area);
